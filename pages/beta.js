f854e08fe66070998c474410f7fe72f6e7b53bcd
JiundJi <62948854+JiundJi@users.noreply.github.com>
2023-02-01 18:47:29 +0100

    the beta is up :3

---
+++ pages/beta.js
@@ -0,0 +1,81 @@
+/* 
+var input = document.getElementById("input");
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("enterKey").click();
+  }
+}); 
+*/
+
+function init() {
+    document.getElementById("o1").innerText = "Enter 'help' for help!";
+}
+
+function trigger() {    
+    makeRoom();
+    
+    let termOut = action();
+
+    document.getElementById("o1").innerText = "Your input: " + document.getElementById("termInput").value + "\n ------------- \n" + termOut;
+    document.getElementById("termInput").value = "";
+    
+    return false;
+}
+
+function action() {
+    let command = document.getElementById("termInput").value.split(" ");
+    if (command[0] == "goto") {
+        return showContent(command[1]);
+    }
+    else if (command[0] == "help") {
+        return "i am a terminal. you give me commands, i do something... i guess \n try 'commands' to get a list of commands!";
+    } else if (command[0] == "toggleMode") {
+        makeRoom();
+        return "dont";
+    } else if (command[0] == "commands") {
+        return "here is a list of commands: \n - help: shows you what this is \n - commands: shows all the commands available \n - goto xyz: show the page 'xyz' \n - toggleMode: toggles darkmode/whitemode";
+    } else {
+        return errorMessage();
+    }
+    
+}
+
+function makeRoom() {
+    document.getElementById("o9").innerText = document.getElementById("o8").innerText;
+    document.getElementById("o8").innerText = document.getElementById("o7").innerText;
+    document.getElementById("o7").innerText = document.getElementById("o6").innerText;
+    document.getElementById("o6").innerText = document.getElementById("o5").innerText;
+    document.getElementById("o5").innerText = document.getElementById("o4").innerText;
+    document.getElementById("o4").innerText = document.getElementById("o3").innerText;
+    document.getElementById("o3").innerText = document.getElementById("o2").innerText;
+    document.getElementById("o2").innerText = document.getElementById("o1").innerText;
+}
+
+function showContent(content) {
+
+    switch (content) {
+        case "home": document.getElementById("content").innerHTML='<object type="text/html" data="../index.html" ></object>'; break;
+        case "coding": document.getElementById("content").innerHTML='<object type="text/html" data="../pages/coding.html" ></object>'; break;
+        case "about_me": document.getElementById("content").innerHTML='<object type="text/html" data="../pages/about_me.html" ></object>'; break;
+        case "music": document.getElementById("content").innerHTML='<object type="text/html" data="../pages/music.html" ></object>'; break;
+        default: return "an error has occured. check for spelling";
+    }
+
+    return "here you go :3";
+
+}
+
+function errorMessage() {
+    let x = Math.random() * 101;
+    if (x <= 25) {
+        return "wat";
+    } else if (x <= 50) {
+        return "please explain what you just said (not actually)";
+    } else if (x <= 75) {
+        return "i dont know what to do with this";
+    } else if (x <= 100) {
+        return "try using 'help'!";
+    }
+}
